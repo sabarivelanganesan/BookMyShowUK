70627777b698904d078cb3af956074bb96f56bc7
Sabarivelan Ganesan <sabarivelangn@gmail.com>
2024-02-08 11:03:58 +0000

    Add project files.

---
+++ EMovieTickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMovieTickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
